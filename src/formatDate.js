5ad238bd2a42c4b1f71a4c26963f10d7c4b7c4d6
Delviiiii <vladbuld71@gmail.com>
2026-09-25 13:30:52 +0200

    solution v0.2

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,74 +8,44 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  function formatYear(initialYear, finalYear) {
-    if (initialYear.length !== finalYear.length) {
-      if (initialYear.length === 4) {
-        return `${initialYear[2]}${initialYear[3]}`;
-      }
+  function formatYear(neededYearFormat) {
+    const keys = Object.keys(initialDate);
+    const initialYear = keys.find((prop) => prop.includes('Y'));
 
+    if (initialYear.length !== neededYearFormat.length) {
       if (initialYear.length === 2) {
-        if (+initialYear < 30) {
-          return `20${initialYear[0]}${initialYear[1]}`;
+        if (initialDate[initialYear] < 30) {
+          return `20${initialDate[initialYear]}`;
         } else {
-          return `19${initialYear[0]}${initialYear[1]}`;
+          return `19${initialDate[initialYear]}`;
         }
+      } else {
+        return initialDate[initialYear].slice(2);
       }
     }
 
-    return initialYear;
+    return initialDate[initialYear];
   }
 
   const fromSeparator = fromFormat[3];
-  const convertedDate = date.split(fromSeparator);
-  let year;
-  let month;
-  let day;
-
-  // Определяет изначальный формат даты и конвертирует её в массив
-  if (fromFormat[0].includes('Y') && fromFormat[1].includes('M')) {
-    [year, month, day] = convertedDate;
-  }
-
-  if (fromFormat[0].includes('M') && fromFormat[1].includes('Y')) {
-    [month, year, day] = convertedDate;
-  }
-
-  if (fromFormat[0].includes('M') && fromFormat[1].includes('D')) {
-    [month, day, year] = convertedDate;
-  }
-
-  let formatedDate = '';
+  const toSeparator = toFormat[3];
+  const partsOfDate = date.split(fromSeparator);
+  const initialDate = {};
   const result = [];
 
-  // Формирует массив согласно переданому формату
-  if (toFormat[0].includes('Y') && toFormat[1].includes('M')) {
-    // Функция для конвертации года в две стороны
-    year = formatYear(year, toFormat[0]);
-
-    result.push(year, month, day);
-    formatedDate = result.join(toFormat[3]);
-
-    return formatedDate;
+  for (let i = 0; i < partsOfDate.length; i++) {
+    initialDate[fromFormat[i]] = partsOfDate[i];
   }
 
-  if (toFormat[0].includes('D') && toFormat[1].includes('M')) {
-    year = formatYear(year, toFormat[2]);
-
-    result.push(day, month, year);
-    formatedDate = result.join(toFormat[3]);
-
-    return formatedDate;
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    if (toFormat[i].includes('Y')) {
+      result.push(formatYear(toFormat[i]));
+    } else {
+      result.push(initialDate[toFormat[i]]);
+    }
   }
 
-  if (toFormat[0].includes('M') && toFormat[1].includes('D')) {
-    year = formatYear(year, toFormat[2]);
-
-    result.push(month, day, year);
-    formatedDate = result.join(toFormat[3]);
-
-    return formatedDate;
-  }
+  return result.join(toSeparator);
 }
 
 module.exports = formatDate;
